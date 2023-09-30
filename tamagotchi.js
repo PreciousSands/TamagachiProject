88f22c24eaacce4ee7c27ac4388ed85a6322a6b6
PreciousSands <precious.sands15@gmail.com>
2023-09-30 16:25:55 -0300

    second commit

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,11 +1,15 @@
 // alert("File is linked")
 class Tamagotchi {
-    constructor(name,age, eyes, bodyColor) {
+    constructor(name,age, eyes, bodyColor, armColor, legColor) {
         this.legs = 2;
         this.arms = 2;
         this.name = name
         this.age = age;
         this. eyes = eyes;
-        this.bodyColor = bodyColor
+        this.bodyColor = bodyColor;
+        this.armColor = armColor;
+        this.legColor = legColor
     }
 }
+const emma = new Tamagotchi('Emma', 5, 'Brown', 'Pink', 'Black', 'Black')
+console.log(emma)
