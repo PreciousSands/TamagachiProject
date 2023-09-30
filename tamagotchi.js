cffc1092aa2f5b3132b8154ee98cf470a20752fb
PreciousSands <precious.sands15@gmail.com>
2023-09-30 18:20:42 -0300

    third commit

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,15 +1,33 @@
 // alert("File is linked")
 class Tamagotchi {
-    constructor(name,age, eyes, bodyColor, armColor, legColor) {
-        this.legs = 2;
-        this.arms = 2;
-        this.name = name
-        this.age = age;
-        this. eyes = eyes;
-        this.bodyColor = bodyColor;
-        this.armColor = armColor;
-        this.legColor = legColor
+    constructor(hunger,sleepiness,boredom,age) {
+        this.hunger = hunger;
+        this.sleepiness =sleepiness;
+        this.boredom = boredom;
+        this.age = age
     }
 }
-const emma = new Tamagotchi('Emma', 5, 'Brown', 'Pink', 'Black', 'Black')
-console.log(emma)
+const milktchi = new Tamagotchi('Milktchi', 5, 'Brown', 'Pink', 'Black', 'Black')
+console.log(milktchi)
+
+// Trying out jquery because i really wanted a fade in 
+
+let opacity = 0;
+        let intervalID = 0;
+        window.onload = fadeIn;
+          
+        function fadeIn() {
+            setInterval(show, 200);
+        }
+          
+        function show() {
+            let h1 = document.getElementById("h1");
+            opacity = Number(window.getComputedStyle(h1)
+                            .getPropertyValue("opacity"));
+            if (opacity < 1) {
+                opacity = opacity + 0.1;
+                h1.style.opacity = opacity
+            // } else {
+            //     clearInterval(intervalID);
+            }
+        }
