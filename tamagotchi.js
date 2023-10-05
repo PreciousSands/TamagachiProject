d5eec2971ade345a7ba956134bf8eec30324b2a6
PreciousSands <precious.sands15@gmail.com>
2023-10-05 00:54:55 -0300

    morphed pet at certain ages

--- tamagotchi.js
+++ tamagotchi.js
@@ -43,29 +43,20 @@ document.addEventListener("DOMContentLoaded", function () {
         gameInterval: setInterval(updateStats, 5000),
         isSleeping: false,
     };
-   
-    // setNameButton.addEventListener("click", function () {
-    //     const newName = petNameInput.value;
-    //     if (newName.trim() !== "") {
-    //         pet.name = newName;
-    //         tamagotchiName.textContent = 'Welcome to the world of ${pet.name}';
-    //     }
-    // setNameButton.addEventListener("click", changePetName)
 
+    // Functions for my eat, play and sleep button
     eatButton.addEventListener("click", function () {
         if (pet.hunger < 100) {
             pet.hunger += 10;
             updateStats();
         }
     });
-
     playButton.addEventListener("click", function () {
         if (pet.boredom < 100) {
             pet.boredom += 10;
             updateStats();
         }
     });
-
     sleepButton.addEventListener("click", function () {
         if (pet.sleepiness < 100) {
             pet.sleepiness += 10;
@@ -73,25 +64,31 @@ document.addEventListener("DOMContentLoaded", function () {
             toggleLights();
         } 
     })
-
     setNameButton.addEventListener("click", function () {
             const newName = prompt("Enter new name for your pet:");
             if (newName !== null && newName.trim() !== "") {
                 pet.name = newName;
                 tamagotchiName.textContent = `Welcome to the world of ${pet.name} !`;
             }
-    // function changePetName() {
-    //     const newName = prompt("Enter a new name for your pet:");
-    //     if (newName !== null && newName.trim() !== "") {
-    //         pet.name = newName;
-    //         tamagotchiName.textContent = `Welcome to the world of ${pet.name}!`;
-    //     }
     });
+
+    function changePetImageByAge() {
+        if (pet.age > 15) {
+            document.getElementById("pet-image").src ="images/Adult_milktchi-r.png"
+        } else if (pet.age > 8) {
+            document.getElementById("pet-image").src ="images/MilktchiFashionCollab.PNG.webp 1.webp"
+        } else {
+            document.getElementById("pet-image").src ="images/Milktchi_animated_sprite.webp"
+        }
+    }
+    // function to increase my pet's age
     function increaseAge() {
         pet.age++;
         ageDisplay.textContent = pet.age;
+        changePetImageByAge();
     }
 
+    // function so that the status bars can deplete unless clicked to refill
     function updateStats() {
         pet.hunger = Math.max(0, pet.hunger - 5) ;
         pet.boredom = Math.max(0, pet.boredom - 5);
